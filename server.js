b5aa322338ebad67af5145fabf3f91819e32d272
prashanthkeetha <124016434+prashanthkeetha@users.noreply.github.com>
2023-10-03 14:52:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 const route = require("./route");
 
 app.use(bodyParser.json())
-
 app.use(bodyParser.urlencoded({extended :true}))
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/movies',route)
 
 app.listen(process.env.PORT,'0.0.0.0',()=>{
